93b4d0e2814d6d733b1d8cd37c242b950fe09ee2
dajun.jiang <dajun.jiang@nio.com>
2022-04-27 16:02:19 +0800

    修改移动端应用

--- build/config-page.js
+++ build/config-page.js
@@ -1,14 +1,9 @@
-const argv = process.argv;
-// 去最后一个值
-const md = argv[argv.length - 1];
-console.log(md);
-process.env.VUE_APP_MODULE = md;
 const pages = {
   index: {
     entry: "src/main.js",
     template: "public/index.html",
     filename: "index.html",
-    title: "蔚站",
+    title: "移动端应用",
     chunks: ["chunk-vendors", "chunk-common", "index"],
   },
 };
