48ae48941f66d2b5dd8e9891a80091a2c13fe777
dajun.jiang <dajun.jiang@nio.com>
2022-05-23 11:58:52 +0800

    修改配置

--- build/config-dev-server.js
+++ build/config-dev-server.js
@@ -1,11 +1,11 @@
 const devServer = {
   proxy: {
-    "/weiH5Api": {
+    "/": {
       // 测试接口
-      target: "http://10.112.32.223:8001",
+      target: "http://****:8001",
       changeOrigin: true,
       pathRewrite: {
-        "^/weiH5Api": "/",
+        "^/": "/",
       },
     },
   },
